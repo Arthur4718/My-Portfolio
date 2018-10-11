8801648b2091072baf966a5c2587d1548584bf0d
Unknown <arthur.gomes_4718@hotmail.com>
2018-10-10 23:53:05 -0300

    WorkView Fragment.
    This is the new page that can display some new information about works that i've done.

--- app/src/main/java/devarthur/com/myportfoolio/view/skillsFragment.java
+++ app/src/main/java/devarthur/com/myportfoolio/view/skillsFragment.java
@@ -9,9 +9,7 @@ import android.view.ViewGroup;
 
 import devarthur.com.myportfoolio.R;
 
-/**
- * A simple {@link Fragment} subclass.
- */
+
 public class skillsFragment extends Fragment {
 
 
@@ -24,7 +22,8 @@ public class skillsFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_skills, container, false);
+        View skillsView = inflater.inflate(R.layout.fragment_skills, container, false);
+        return skillsView;
     }
 
 }
