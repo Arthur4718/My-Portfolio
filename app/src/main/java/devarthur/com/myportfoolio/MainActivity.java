09374013724ba592dc393001ccec1117f50b46e2
Unknown <arthur.gomes_4718@hotmail.com>
2018-10-11 11:19:57 -0300

    Get in touch button.
    Added a code to start the activity intent for the dial. Added a string resource to hold up the phone number.

--- app/src/main/java/devarthur/com/myportfoolio/MainActivity.java
+++ app/src/main/java/devarthur/com/myportfoolio/MainActivity.java
@@ -1,15 +1,20 @@
 package devarthur.com.myportfoolio;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 import devarthur.com.myportfoolio.controller.MainPageAdapter;
 
 public class MainActivity extends AppCompatActivity {
 
     private ViewPager viewPager;
+    private TextView getInTouch;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,5 +26,15 @@ public class MainActivity extends AppCompatActivity {
 
         TabLayout tabs = (TabLayout) findViewById(R.id.tabLayoutID);
         tabs.setupWithViewPager(viewPager);
+
+        getInTouch = (TextView) findViewById(R.id.getInTouch);
+
+        getInTouch.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String dial = "tel:" + getString(R.string.mynumber);
+                startActivity(new Intent(Intent.ACTION_DIAL, Uri.parse(dial)));
+            }
+        });
     }
 }
